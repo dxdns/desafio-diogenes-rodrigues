9949b2011fac620781500b44b57da0f87414c66f
dxdns <dxdns@hotmail.com>
2023-08-13 19:20:17 -0300

    add comments & methods

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -2,74 +2,104 @@ import { cardapio, formasPagamento } from "./data.js";
 
 class CaixaDaLanchonete {
   calcularValorDaCompra(metodoDePagamento, itens) {
-    if (itens.length == 0) {
+    // Verifica se há itens no carrinho.
+    if (itens.length === 0) {
       return "Não há itens no carrinho de compra!";
     }
 
-    let mPagamento = formasPagamento[metodoDePagamento];
+    // Obtém as informações do método de pagamento.
+    const mPagamento = formasPagamento[metodoDePagamento];
 
-    if (mPagamento === undefined) {
+    // Verifica se o método de pagamento é válido.
+    if (!mPagamento) {
       return "Forma de pagamento inválida!";
     }
 
-    let { pDesconto, pAcrescimo } = mPagamento;
+    // Extrai os valores de desconto e acréscimo do método de pagamento.
+    const { pDesconto, pAcrescimo } = mPagamento;
 
-    let valorTotal = 0;
+    let valorTotalPedido = 0;
+    let valorTotalItens = 0;
+    let produtos = [];
 
-    let quantidadeProdutos = 0;
-    let somaValorItens = 0;
-    let apenasItens = [];
-    let extra;
+    const extras = [];
 
-    itens.map((v, i) => {
-      let [produto, quantidade] = v.split(",");
-      apenasItens = [...apenasItens, produto];
-      let cardapioProduto = cardapio[produto];
-      
-      quantidadeProdutos += quantidade;
+    // Loop através dos itens no carrinho.
+    for (const item of itens) {
+      // Divide a string do item em produto e quantidade.
+      const [produto, quantidade] = item.split(",");
 
-      let valorProduto = cardapioProduto.valor;
-      extra = cardapioProduto.extra;
+      // Obtém as informações do produto a partir do cardápio.
+      const cardapioProduto = cardapio[produto];
 
-      somaValorItens += valorProduto * quantidade;
+      // Verifica se o produto é válido.
+      if (!cardapioProduto) {
+        return "Item inválido!";
+      }
 
-      let valorDesconto = (somaValorItens * pDesconto) / 100;
-      let valorAcrescimo = (somaValorItens * pAcrescimo) / 100;
+      // Obtém o valor do produto.
+      const valorProduto = cardapioProduto.valor;
 
-      valorTotal = somaValorItens - valorDesconto + valorAcrescimo;
+      // Verifica se a quantidade é um número válido ou se é menor ou igual a zero.
+      if (isNaN(quantidade) || quantidade <= 0) {
+        return "Quantidade inválida!";
+      }
 
-      // console.log(produto, quantidade, quantidade == 0);
-      // console.log(`${produto}, ${metodoDePagamento}, valor: ${valorProduto}, desconto: ${pDesconto}, acrescimo: ${pAcrescimo}`);
-      // console.log(`produto: ${produto} valor: ${valorProduto}`)
-      // console.log(`itens: ${itens}, somaValorItens: ${somaValorItens}`)
-      // console.log(`desconto: ${valorDesconto}, acrescimo: ${valorAcrescimo} total: ${valorTotal}`);
+      // Adiciona o nome dos produtos no array.
+      produtos.push(produto);
 
-      console.log("-----------------------------------------------------");
-    });
+      // Calcula o valor total dos itens.
+      valorTotalItens += valorProduto * quantidade;
 
-    if (quantidadeProdutos == 0) {
-      return "Quantidade inválida!";
+      // Verifica se o produto é um item extra e o adiciona ao array de extras.
+      if (cardapioProduto.extra) {
+        extras.push(cardapioProduto.extra);
+      }
     }
 
-    console.log(extra);
-    console.log(apenasItens);
+    // Verifica se pelo menos um item no carrinho possui um item principal correspondente.
+    if (!produtos.some((item) => this.temItemPrincipal(item, produtos))) {
+      return "Item extra não pode ser pedido sem o principal";
+    }
 
-    // if (!apenasItens.includes(extra)) {
-    //   return "Item extra não pode ser pedido sem o principal";
-    // }
+    // Filtra os extras que não possuem um item principal correspondente.
+    const itensPrincipaisAusentes = extras.filter(
+      (extra) => !produtos.includes(extra)
+    );
 
-    // if (extra !== undefined) {
-    //   // console.log(extra);
-    //   // console.log(apenasItens);
-    //   // console.log(!apenasItens.includes(extra));
-    //   // console.log("Item extra não pode ser pedido sem o principal");
+    // Verifica se há extras sem um item principal correspondente.
+    if (itensPrincipaisAusentes.length > 0) {
+      return "Item extra não pode ser pedido sem o principal";
+    }
 
-    //   if (!apenasItens.includes(extra)) {
-    //     return "Item extra não pode ser pedido sem o principal";
-    //   }
-    // }
+    // Calcula o valor do desconto e acréscimo.
+    const valorDesconto = (valorTotalItens * pDesconto) / 100;
+    const valorAcrescimo = (valorTotalItens * pAcrescimo) / 100;
 
-    return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
+    // Calcula o valor total do pedido.
+    valorTotalPedido = valorTotalItens - valorDesconto + valorAcrescimo;
+
+    return this.formatarValor(valorTotalPedido);
+  }
+
+  /**
+   * Verifica se um item extra é pedido junto com o item principal correspondente.
+   * @param {string} item - Nome do item que está sendo verificado.
+   * @param {string[]} produtos - Array apenas com o nome dos produtos.
+   * @returns {boolean} True se o item extra for pedido com o item principal correspondente, caso contrário, False.
+   */
+  temItemPrincipal(item, produtos) {
+    const produto = cardapio[item];
+    return !produto.extra || produtos.includes(produto.extra);
+  }
+
+  /**
+   * Formata o valor total do pedido como uma string no estilo monetário, com duas casas decimais.
+   * @param {number} valorTotalPedido - O valor total do pedido a ser formatado.
+   * @returns {string} Uma string formatada no estilo monetário, por exemplo: "R$ 123,45".
+   */
+  formatarValor(valorTotalPedido) {
+    return `R$ ${valorTotalPedido.toFixed(2).replace(".", ",")}`;
   }
 }
 
